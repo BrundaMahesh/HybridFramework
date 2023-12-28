2155b9a327d70d26b0a884d8d602e277b54ae012
BrundaMahesh <brunda.mahesh@ust.com>
2023-12-28 12:10:02 +0530

    commit

---
+++ Google/Selenium Tests/Pages/HomePage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Selenium_Tests.Pages
+{
+    internal class HomePage : BasePage
+    {
+        [CacheLookup]
+        private IWebElement? SearchBox => Driver?.FindElement(By.Name("q"));
+
+        [CacheLookup]
+        [FindsBy(How = How.Name,Using = "btnK")]
+        private IWebElement? SearchButton { get; set; }
+
+        public HomePage(IWebDriver driver):base(driver) { }
+
+        public void EnterSearchText(string? searchText)
+        {
+            SearchBox?.SendKeys(searchText);
+        }
+
+        public void ClickSearchButton()
+        {
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(Driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(5);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(100);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fluentWait.Message = "Element not found";
+
+            fluentWait.Until(x => x.FindElement(By.Name("btnK")));
+            SearchButton?.Click();
+        }
+    }
+}
---
+++ Google/Selenium Tests/Tests/HomePageTests.cs
@@ -0,0 +1,21 @@
+﻿using Google.Selenium_Tests.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Selenium_Tests.Tests
+{
+    internal class HomePageTests : BasePage
+    {
+        private HomePage homePage;
+        string? testName;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+           
+        }
+    }
+}
